3c9da1de9ceec3e2bf0550f3d4b65fa98292b5f3
Steven Thacker <s.v.thacker@gmail.com>
2014-11-05 19:06:32 +0900

    working c++ hello world

--- OPENGL/main.cpp
+++ OPENGL/main.cpp
@@ -1,13 +1,16 @@
 //
-//  main.m
+//  main.cpp
 //  OPENGL
 //
 //  Created by Bilbo Baggins on 11/5/14.
 //  Copyright (c) 2014 Steven Thacker. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    return NSApplicationMain(argc, argv);
+
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello World!";
+    return 0;
 }
