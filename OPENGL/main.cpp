3762582f5f91be897c628aaf56e112e670196fa4
Steven Thacker <s.v.thacker@gmail.com>
2014-11-05 20:29:51 +0900

    adds libraries which seem to work

--- OPENGL/main.cpp
+++ OPENGL/main.cpp
@@ -6,11 +6,23 @@
 //  Copyright (c) 2014 Steven Thacker. All rights reserved.
 //
 
+#define GLFW_INCLUDE_GLCOREARB
 #include <iostream>
-
+#include </opt/local/include/GLFW/glfw3.h>
+#include </opt/local/include/glm/glm.hpp>
 
 int main(int argc, const char * argv[])
 {
-    std::cout << "Hello World!";
+    GLint glfwStatus = glfwInit();
+    if (glfwStatus != GL_TRUE)
+    {
+        std::cout << "GLFW Failed to Initialize!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+    
+    
+    
+    glfwTerminate();
     return 0;
 }
